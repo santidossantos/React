82c8ccb3f97f844113e50e38337df531d1f3478e
Santiago dos Santos <santiiagodossantos@icloud.com>
2023-02-11 17:24:48 -0300

    cambios

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
 
 function App() {
+
+  //var codigoHTML = <h1>Titulo</h1> // Podemos guardar html en una variable
+
+  const [linea1, setLinea1] = useState('')
+  const [linea2, setLinea2] = useState('')
+
+  function cambio() {
+    debugger;
+    alert()
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello <br />
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <select>
+        <option value="meme1">Meme 1</option>
+        <option value="meme2">Meme 2</option>
+        <option value="meme3">Meme 3</option>
+      </select> <br />
+     
+      <input onChange={cambio} type='text'/> <br />
+      <input type='text'/> <br />
+      <button>Exportar</button>
+
+      <div>
+        <span>{linea1}</span> <br />
+        <span>{linea2}</span> <br />
+        <img src='' alt='Meme'/>
+      </div>
+
     </div>
   );
 }
